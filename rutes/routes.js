36cc7e3858966bfa1d15c22d238b57d3a52da4a0
Alexander R <alexandereliasr9@gmail.com>
2019-10-07 23:24:56 -0400

    this is the second

--- rutes/routes.js
+++ rutes/routes.js
@@ -2,18 +2,38 @@ var express = require('express')
 var router = express.Router()
 const date = require('date-and-time');
 
-// middleware that is specific to this router
-router.use(function timeLog (req, res, next) {
+//funtions for anything
+function timeLog () {
   const now = new Date();
   var log_date = date.format(now, 'ddd. YYYY/MM/DD HH:mm:ss');
   console.log("Someone vistit the page at "+log_date);
-  next()
-})
+}
 
+function sendMsg(msg) {
+  const accountSid = 'AC4234120e92e3867a93bc8f6023e34ad9';
+  const authToken = 'dfb00522ddb45141768cdc6898e5e38c';
+  const client = require('twilio')(accountSid, authToken);
+
+  client.messages
+        .create({
+           from: 'whatsapp:+14155238886',
+           body: msg,
+           to: 'whatsapp:+18496573947'
+         })
+        .then(message => console.log(message));
+
+}
+
+function save(json) {
+  var info = require('../datos.json')
+  info.push(json);
+  console.log(info);
+}
 
 /*---------------Log web page--------------*/
 router.get('/', function (req, res) {
   res.render("index.ejs")
+  timeLog()
 });
 
 router.post('/login', (req,res) =>{
@@ -34,12 +54,14 @@ router.post('/form', (req,res) => {
   var name = req.body.name
   var age = req.body.age
   var nationality = req.body.nationality
+  var platform = req.body.platform
   const now = new Date();
   var log_date = date.format(now, 'ddd. YYYY/MM/DD HH:mm:ss');
   var result ={
     name:name,
     age:age,
     nationality:nationality,
+    platform:platform,
     date:log_date
   }
   console.log(result);
